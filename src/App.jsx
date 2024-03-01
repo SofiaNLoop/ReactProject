aff27e8a33acbdf7b61593d95e587f5e7cd002be
Sofía <sofia.neira@ias.com.com>
2024-03-01 09:57:47 -0500

    components folder created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import JobCard from './components/JobCard'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <JobCard></JobCard>
         <h1 className='bg-blue-500 text-black text 3xl' > Comenzando proyecto </h1>
       </div>
     </>
