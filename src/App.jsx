ed2ec543c5b4ce7c9b56cdbd52567f197c8b2886
Sofía <sofia.neira@ias.com.com>
2024-03-02 12:39:48 -0500

    add const pag

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,12 @@ import './App.css'
 import JobCard from './components/JobCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <JobCard></JobCard>
-        <h1 className='bg-blue-500 text-black text 3xl' > Comenzando proyecto </h1>
+
       </div>
     </>
   )
